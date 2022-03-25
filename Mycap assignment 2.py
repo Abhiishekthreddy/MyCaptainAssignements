9f9cc1c1e5ec150928c0c1e1a82db1e118df2d76
Abhiishekthreddy <61532832+Abhiishekthreddy@users.noreply.github.com>
2022-03-25 22:17:21 +0530

    Add files via upload

---
+++ Mycap assignment 2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Filename = input("Input the file name ")
+x= ".py"
+y= ".java"
+z= ".c"
+a= ".CPP"
+if x in Filename:
+    print("Python")
+elif y in Filename:
+        print("Java")
+elif z in Filename:
+        print("C")    
+elif a in Filename:
+        print("C++")
+    
+
