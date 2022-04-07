e3dc3d61c96775b47a4cda69a9fae073d58b8db4
Abhiishekthreddy <61532832+Abhiishekthreddy@users.noreply.github.com>
2022-04-08 00:15:08 +0530

    Add files via upload

---
+++ printing positive elements of list.py
@@ -0,0 +1,10 @@
+lst = []
+lst1 = []
+i = int(input("Enter the no.of elements in the list "))
+for i in range(i):
+    x= int(input("Enter the element "))  
+    lst.append(x)  
+for j in lst:
+    if j>0:
+        lst1.append(j)
+print(lst1)
